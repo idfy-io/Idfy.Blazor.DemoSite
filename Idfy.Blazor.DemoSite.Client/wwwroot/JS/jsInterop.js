65f8671e42d252bbb023843a939d200f3fc45fb5
vPolland <vegard@idfy.io>
2019-08-12 08:02:30 +0200

    Electron fix

---
+++ Idfy.Blazor.DemoSite.Client/wwwroot/JS/jsInterop.js
@@ -0,0 +1,34 @@
+﻿window.blazorExtras = {
+    readUploadedFileAsText: function (fileInputId) {
+
+        return new Promise((resolve, reject) => {
+            const file = document.getElementById(fileInputId).files[0];
+            if (file) {
+                var reader = new FileReader();
+                reader.onerror = () => {
+                    reader.abort();
+                    reject(new DOMException("Problem parsing input file."));
+                };
+                reader.onload = function (evt) {
+                    var data = {
+                        content: evt.target.result,
+                        name: file.name
+                    };
+                    resolve(data);
+                };
+                reader.readAsDataURL(file);
+            };
+        });
+    },
+    openUrlExternal: function (url) {
+        //Electron
+        var isElectron = window && window.process && window.process.type;
+        if (isElectron) {
+            let shell = require('electron').shell
+            shell.openExternal(url);
+        } else {
+            window.open(url, "_blank");
+        }
+    }
+};
+
